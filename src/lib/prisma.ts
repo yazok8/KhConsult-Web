a3c16e5bc1c04cbd752320f1390091411d825bb5
yazok8 <yazankhirfan@gmail.com>
2024-12-21 14:21:35 -0500

    remove comment from prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
 export const prisma =
