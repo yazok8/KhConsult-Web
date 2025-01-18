5434537f96d610a308e2eb4978d541ead1f19715
yazok8 <yazankhirfan@gmail.com>
2025-01-18 09:42:35 -0500

    remove link import from faq page

--- src/app/(client)/pages/faq/page.tsx
+++ src/app/(client)/pages/faq/page.tsx
@@ -3,7 +3,6 @@
 export const dynamic = "force-dynamic";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import sanitizeHtml from "sanitize-html";
 
