b71be5355131dba7b4a5d5b50e1b113077e08253
yazok8 <yazankhirfan@gmail.com>
2025-02-02 15:30:34 -0500

    Trigger Build

--- src/app/(client)/pages/services/page.tsx
+++ src/app/(client)/pages/services/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import Container from "@/components/Container";
 import ForBusinesses from "./ForBusinesses/page";
 import StudyingInGermany from "./studying-in-germany/page";
