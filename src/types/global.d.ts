c7a1ed1702ec2f6617f43df38e5c1e5ce6533e3d
Yazan <ykherfan8@gmail.com>
2025-02-18 23:11:07 +0300

    apply more specific gtag type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,14 @@
 export {};
 
+// Define a type for the gtag function with overloads for common commands.
+type Gtag = {
+  (command: 'js', config: Date): void;
+  (command: 'config', targetId: string, config?: Record<string, unknown>): void;
+  (command: 'event', eventName: string, eventParams?: Record<string, unknown>): void;
+};
+
 declare global {
   interface Window {
-    gtag: (...args: any[]) => void;
+    gtag: Gtag;
   }
 }
